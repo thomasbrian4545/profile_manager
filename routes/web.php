d1e69f809003222a2f39395d7c7527d6af1e0bc7
thomasbrian4545 <thomasbrian4545@gmail.com>
2023-10-03 22:09:12 +0700

    Membuat controller user dan modif route

--- routes/web.php
+++ routes/web.php
@@ -1,22 +1,19 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
-
-Route::get('/', function () {
-    return view('welcome');
-});
+use Illuminate\Support\Facades\Auth;
+use App\Http\Controllers\UserController;
 
 Auth::routes();
 
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+Route::get('/', [App\Http\Controllers\HomeController::class, 'index'])
+->name('home');
+
+Route::get('/users/{user}/edit', [UserController::class,'edit'])
+->middleware('can:update,user');
+
+Route::put('/users/{user}', [UserController::class,'update'])
+->middleware('can:update,user');
+
+Route::delete('/users/{user}', [UserController::class,'destroy'])
+->middleware('can:delete,user');
